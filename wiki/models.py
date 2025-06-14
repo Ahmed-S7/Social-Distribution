109f9744419080f98e27d221073d4eb44a77f87d
Ahmed <ashittu38@gmail.com>
2025-06-14 00:32:36 -0600

    Added Author Model
    - updated registration so that it adds a new author upon registration
    - created the author model
    - created the author serializer
    - fixed corrupted requirements.txt
    - added extra check for valid username
    - authors are now created in conjunction with users, this way we can differentiate a user from an author while keeping the username and password authentication from Django's user model
    - added a util file for extra functions to prevent clutter in views.py

--- wiki/models.py
+++ wiki/models.py
@@ -26,3 +26,54 @@ class RemotePost(models.Model):
     received_at = models.DateTimeField(auto_now_add=True)
 
 
+class Author(models.Model):
+    """
+    **Model that represents an author object in the application**\n
+    *Fields*:\n
+    id: the full API URL for the author (the path to the author's page on their given node)\n
+    host: the full API URL for the author's node (the path to the author's node)\n
+    displayName: the author's chosen username\n
+    github: the (optional) user's github profile\n
+    profileImage: the URL of the user's profile image\n
+    web: the URL to the user's page on their node\n
+    
+
+    **Inherits from the default user model with these fields**:\n
+    
+    username – Unique identifier (max 150 characters).\n  
+    
+    id – Auto-generated primary key.\n 
+
+    password – Hashed password storage.\n 
+
+    first_name – Optional first name.\n 
+
+    last_name – Optional last name.\n 
+
+    email – Email address.\n 
+
+    is_staff – Boolean flag for admin privileges.\n 
+
+    is_active – Determines if the account is active.\n 
+
+    is_superuser – Grants all permissions.\n 
+
+    last_login – Timestamp of last login.\n 
+
+    date_joined – Timestamp of account creation.\n 
+    """
+    type = models.CharField(default="author")
+    
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
+     
+    authorURL = models.URLField(unique=True)# formatted as: "http://white/api/authors/[authorID]"
+     
+    host = models.URLField(default=f"http://s25-project-white/api/")
+    
+    displayName = models.CharField(max_length=150)
+    
+    github = models.URLField(blank=True, null=True, default=None)
+    
+    profileImage  = models.URLField(blank=True, null=True, default="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y")
+    
+    web = models.URLField(blank=True, null=True, default=None)
--- wiki/serializers.py
+++ wiki/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from .models import Page, Like, RemotePost
+from .models import Page, Like, RemotePost, Author
+from django.contrib.auth.models import User
 
 class PageSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField()
@@ -17,3 +18,12 @@ class RemotePostSerializer(serializers.ModelSerializer):
     class Meta:
         model = RemotePost
         fields = '__all__'
+
+class AuthorSerializer(serializers.ModelSerializer):
+    id = serializers.URLField(source="authorURL")
+
+    class Meta:
+        model= Author
+        fields = ["type", "id", "host", "displayName", "github", "profileImage", "web"]
+        
+        
