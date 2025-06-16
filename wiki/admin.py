bb0e5d5bb2c74f7e75628ed2dd9931827472bc63
Ahmed <ashittu38@gmail.com>
2025-06-15 22:55:53 -0600

    new friends model

--- wiki/admin.py
+++ wiki/admin.py
@@ -3,9 +3,52 @@ from .models import Page, Like, RemotePost, Author, FollowRequest, AuthorFollowi
 
 # Register your models here.
 
+
+'''
+class Author(BaseModel):
+    """
+    **Model that represents an author object in the application**\n
+    *Fields*:\n
+    id: the full API URL for the author (the path to the author's page on their given node)\n
+    host: the full API URL for the author's node (the path to the author's node)\n
+    displayName: the author's chosen username\n
+    github: the (optional) user's github profile\n
+    profileImage: the URL of the user's profile image\n
+    web: the URL to the user's page on their node\n
+    **Associated with an accompanying User object to keep username and password consistency**:
+    """
+    type = models.CharField(default="author")
+    
+    user = models.OneToOneField(User, on_delete= models.CASCADE)
+     
+    authorURL = models.URLField(unique=True)# formatted as: "http://white/api/authors/[authorID]"
+     
+    host = models.URLField(default=f"http://s25-project-white/api/")
+    
+    displayName = models.CharField(max_length=150)
+    
+    github = models.URLField(blank=True, null=True, default=None)
+    
+    serial = models.UUIDField(default=None, null=True, unique=True)
+    
+    profileImage  = models.URLField(blank=True, null=True, default="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y")
+    
+    web = models.URLField(blank=True, null=False, default=None)
+'''
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ["authorURL", "displayName", "is_deleted","github","profileImage","host"]
+    list_filter = ["is_deleted", "host"]
+    list_editable = ['displayName']
+    
+    search_fields = ["displayName", "github"]
+ 
+    
+class FollowRequestAdmin(admin.ModelAdmin):
+    list_display = ("")
+    
 admin.site.register(Page)
 admin.site.register(Like)
 admin.site.register(RemotePost)
-admin.site.register(Author)
 admin.site.register(FollowRequest)
 admin.site.register(AuthorFollowing)
+admin.site.register(Author, AuthorAdmin)
--- wiki/models.py
+++ wiki/models.py
@@ -1,10 +1,46 @@
 from django.db import models
 import uuid
+from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
-
+from django.db.models import Manager, QuerySet
+from django.dispatch import receiver
 # Create your models here.
-class Author(models.Model):
+
+
+#The following soft-deletion logic (AppQuerySet, AppManager and BaseModel) was derived from Medium's article: https://medium.com/@tomisinabiodun/implementing-soft-delete-in-django-an-intuitive-guide-5c0f95da7f0d, June 15, 2025
+class AppQuerySet(QuerySet):
+    def delete(self):
+        self.update(is_deleted=True)
+  
+  
+class AppManager(Manager):
+    def get_queryset(self):
+        return AppQuerySet(self.model, using=self._db).exclude(is_deleted=True)
+  
+  
+class BaseModel(models.Model):
+    class Meta:
+        abstract = True
+  
+    is_deleted = models.BooleanField(default=False)
+    
+    def delete(self):
+        self.is_deleted = True
+        self.save()
+        
+class VisibilityOptions(models.TextChoices):
+    PUBLIC = "public", "Public"
+    UNLISTED = "unlisted", "Unlisted"
+    FRIENDS_ONLY = "friends_only", "Friends_only"
+    DELETED = "deleted", "Deleted"    
+
+class InboxObject(BaseModel):
+    
+    pass
+    
+    
+class Author(BaseModel):
     """
     **Model that represents an author object in the application**\n
     *Fields*:\n
@@ -32,22 +68,54 @@ class Author(models.Model):
     
     profileImage  = models.URLField(blank=True, null=True, default="https://www.gravatar.com/avatar/00000000000000000000000000000000?d=mp&f=y")
     
-    web = models.URLField(blank=True, null=True, default=None)
+    web = models.URLField(blank=True, null=False, default=None)
     
-    def getFollowRequestsSent(self) ->list:
+    def get_follow_requests_sent(self) ->list:
         '''Returns a list of all of the follow requests sent by an author'''
         return list(self.requesting.all())
         
-    def getFollowRequestsRecieved(self)->list:
+    def get_follow_requests_recieved(self)->list:
         '''Returns a list of all of the follow requests recieved by an author'''
         return list(self.follow_requests.all())
     
+    def get_friends(self):
+        '''
+        retrieves a list of a user's friends
+        '''
+        pass
+    
+    
+    def is_friends_with(self, other_author):
+        pass
+        
+        
+        
+        
+    
     def __str__(self):
         return self.displayName
-       
+    
+    def save(self, *args, **kwargs):
+        if self.user.username != self.displayName:
+            self.user.username = self.displayName
+            self.user.save()
+    
+        return super().save(*args,**kwargs)
+     
+    #implemented using code from ChatGPT:https://chatgpt.com/, "How can I get updated author names to be reflected by their user and vice versa?", June 15, 2024
+    @receiver(post_save, sender=User)    
+    def sync_username(sender, instance, **kwargs):
+        '''ensure that any changes to an Author's respective user object are reflected by the author's display name for consistency'''
+        try:
+            author = instance.author
+            if instance.username!= author.displayName:
+                author.displayName = instance.username
+                author.save()
+        except: #only requires an update if the user has an associated author
+            pass
+  
    
-   
-class Page(models.Model):
+class Page(BaseModel):
     title = models.CharField(max_length=100, unique=True)
     content = models.TextField()
     updated = models.DateTimeField(auto_now=True)
@@ -56,27 +124,56 @@ class Page(models.Model):
     def __str__(self):
         return self.title
 
-class Like(models.Model):
+class Like(BaseModel):
     page = models.ForeignKey(Page, on_delete=models.CASCADE, related_name='likes')
     user = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     class Meta:
         unique_together = ('page', 'user')
 
-class RemotePost(models.Model):
+class RemotePost(BaseModel):
     origin = models.URLField()
     author = models.CharField(max_length=100)
     content = models.TextField()
     received_at = models.DateTimeField(auto_now_add=True)
-
- 
     
-#The following model is derived from Stackoverflow.com: https://stackoverflow.com/questions/58794639/how-to-make-follower-following-system-with-django-model, June 14, 2025+ 
-class AuthorFollowing(models.Model):
+    
+
+class AuthorFriend(BaseModel):
+        friending = models.ForeignKey(Author, related_name="friend_a", on_delete=models.CASCADE, null=False)
+        friended = models.ForeignKey(Author, related_name="friend_b", null=False, on_delete=models.CASCADE)
+        friended_at =  models.DateTimeField(auto_now_add=True)
+        
+        #prevents any duplicate friend requests
+        class Meta:
+            constraints = [
+                models.UniqueConstraint(fields=['friending', 'friended'], name='unique_friendship_pair')
+            ]
+            
+        #prevent self-friending
+        def save(self, *args, **kwargs):
+            if self.friending == self.friended:
+                raise ValidationError("You cannot send yourself a friend request")
+
+            #corrected using ChatGPT: "How do I prevent duplicate friendships in reverse order?": https://chatgpt.com/, June 15, 2025
+            #lower id is first so that a friendship A-B cannot also be friendship B-A
+            if self.friending.id > self.friended.id:
+                self.friending, self.friended = self.friended, self.friending
+                
+            super().save(*args, **kwargs)
+            
+        def __str__(self):
+            return f"{self.friending.displayName} Is Friends With {self.friended.displayName}"
+               
+    
+    
+    
+#The following model is derived from Stackoverflow.com: https://stackoverflow.com/questions/58794639/how-to-make-follower-following-system-with-django-model, June 14, 2025+ class AuthorFollowing(BaseModel):
+class AuthorFollowing(BaseModel):
     '''
     Model representing all of the instances where an author is followed or following someone\n
     
-    This is effectively a *followers* and *following* list\n
+    This is effectively a *followers* and *following* list (not stored as a list, but can be queried to retrieve a list)\n
     
     **"following"** in related_name means the list of authors that **"follower" has followed** (the author's following list)\n
     **"followers"** in related_name means a list of the authors that **"following" author has been followed by** (author's follower list\n
@@ -87,6 +184,9 @@ class AuthorFollowing(models.Model):
     
     class Meta:
         unique_together = ("follower", "following")
+        
+        
+    
         
     #derived from stackoverflow.com: https://stackoverflow.com/questions/67658422/how-to-overwrite-save-method-in-django-model-form, "How to overwrite the save method in django model form", June 15, 2025
     def save(self, *args, **kwargs):
@@ -103,7 +203,7 @@ class RequestState(models.TextChoices):
     REJECTED = "rejected", "Rejected"
         
     
-class FollowRequest(models.Model):
+class FollowRequest(BaseModel):
     """**Models a follow Request**\n
     
     *Example Usages:*\n
@@ -115,14 +215,13 @@ class FollowRequest(models.Model):
         - author.followRequests.all()\n\n
         
     Get the state of a given follow request:\n
-        - followrequest
+        - followrequest.get_request_state()
     """
     
     type = models.CharField(default="follow")
     summary = models.CharField(default="You have recieved a follow request!")
     requester = models.ForeignKey(Author, related_name="requesting", on_delete=models.CASCADE, null=False) 
     requested_account = models.ForeignKey(Author, related_name="follow_requests", on_delete=models.CASCADE, null=False)
-    
     state = models.CharField(max_length=15, choices=RequestState.choices, default=RequestState.REQUESTING)
     class Meta:
         unique_together = ("requester", "requested_account")
