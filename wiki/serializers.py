8f51e0900f6cdb0fb6462125e4089f8b22abdc0e
luismjr <luismjr07@gmail.com>
2025-07-29 17:11:45 -0600

    3

--- wiki/serializers.py
+++ wiki/serializers.py
@@ -153,13 +153,13 @@ class LikeSummarySerializer(serializers.Serializer):
         return f"{host}/api/authors/{author_id}/liked/{obj.id}"
 
     def get_object(self, obj):
-        request = self.context.get('request')
-        host = request.build_absolute_uri('/')[:-1] if request else 'http://localhost'
-
+        # Use the entry author's host instead of the current request's host
+        entry_author_host = obj.entry.author.host.rstrip('/')
+        
         # Extract author UUID from the entry's author id URL
         entry_author_id = str(obj.entry.author.id).rstrip('/').split('/')[-1]
         entry_id = obj.entry.serial if hasattr(obj.entry, 'serial') else obj.entry.id
-        return f"{host}/api/authors/{entry_author_id}/entries/{entry_id}"
+        return f"{entry_author_host}/authors/{entry_author_id}/entries/{entry_id}"
 
 
 class CommentLikeSummarySerializer(serializers.Serializer):
@@ -184,11 +184,11 @@ class CommentLikeSummarySerializer(serializers.Serializer):
         return f"{host}/api/authors/{author_id}/liked/{obj.id}"
 
     def get_object(self, obj):
-        request = self.context.get('request')
-        host = request.build_absolute_uri('/')[:-1] if request else 'http://localhost'
-
+        # Use the comment author's host instead of the current request's host
+        comment_author_host = obj.comment.author.host.rstrip('/')
+        
         comment_author_id = str(obj.comment.author.id).rstrip('/').split('/')[-1]
-        return f"{host}/api/authors/{comment_author_id}/commented/{obj.comment.id}"
+        return f"{comment_author_host}/authors/{comment_author_id}/commented/{obj.comment.id}"
 
 
 class CommentSummarySerializer(serializers.Serializer):
