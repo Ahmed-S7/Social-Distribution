a6464710a24686a72eefcf372a53a9ef27fa182e
Ahmed <ashittu38@gmail.com>
2025-07-31 00:54:49 -0600

    fixed image css

--- wiki/views.py
+++ wiki/views.py
@@ -2323,6 +2323,7 @@ def create_entry(request):
 def entry_detail(request, author_serial, entry_serial):
     entry = get_object_or_404(Entry, serial=entry_serial)
     is_owner = (entry.author.user == request.user)
+    print(f"owner of the entry is: {is_owner}")
     current_author = get_object_or_404(Author, serial=author_serial)
     
 
@@ -2334,10 +2335,10 @@ def entry_detail(request, author_serial, entry_serial):
         ).exists()
 
     # if entry is FRIENDS and user is not the owner or a friend, return 403
-    if entry.visibility == 'FRIENDS' and not (is_owner or (request.user.is_authenticated and is_friend)):
-        if not request.user.is_authenticated:
+    if entry.visibility == 'FRIENDS' :
+        if not is_owner and not is_friend:
             return HttpResponse("This entry is private. You must log in to view it.", status=403)
-        else:
+        if not request.user.is_authenticated:
             return HttpResponse("This entry is private. You are not allowed to view it.", status=403)
     comments = entry.comments.filter(is_deleted=False).order_by('created_at')
     #return render(request, 'entry_detail.html', {'entry': entry, 'is_owner': is_owner, 'comments': comments})
